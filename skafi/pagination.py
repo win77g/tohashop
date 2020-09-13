b69ebd359caab426e5079372d43116490fc7d7ce
win21g <win21g@mail.ru>
2020-09-13 18:07:16 +0300

    new main 13.09 18:06

--- skafi/pagination.py
+++ skafi/pagination.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import LimitOffsetPagination,PageNumberPagination
 
 class PostPageNumberPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 15
 
 class PaginationHandlerMixin(object):
     @property
