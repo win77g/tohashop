7d19aeefc22de354f4c5d95e18b1e8e662b97047
win21g <win21g@mail.ru>
2020-10-04 21:34:55 +0300

    new main

--- gostinye/serializers.py
+++ gostinye/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class GostinyeSerializer(serializers.ModelSerializer):
     class Meta:
         model = GostinyeModel
-        fields = ('id','name','keyword','category','podcateg','brend','','image_1','image_2',
+        fields = ('id','name','keyword','category','podcateg','brend','seria','image_1','image_2',
                   'slug','price','height','depth','width','description','description_short',
                   'discount','is_active','new_product','top','photoMenu','slider','comments')
 
@@ -18,6 +18,7 @@ class HeightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Height
         fields = ('name',)
+        
 class WidhtSerializer(serializers.ModelSerializer):
     class Meta:
         model = Width
