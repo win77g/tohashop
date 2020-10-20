d9fe81507e3d9b662340e60c01b410068de0e67e
win21g <win21g@mail.ru>
2020-10-20 12:20:26 +0300

    new main

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = '$2s81)%!49cba64kgl0hbrwk3y0yqfch$a408h##)6u_$6mrij'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 #DEBUG = False
-#ALLOWED_HOSTS = ['www.mebelkom.com.ua']
+ALLOWED_HOSTS = ['www.mebelkom.com.ua']
 
 
 # Application definition
